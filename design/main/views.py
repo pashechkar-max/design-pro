5f59ccfb57f48ab00065090c5b8ee6c941baf403
pashechkar-max <polinausakova921@gmail.com>
2026-02-18 23:01:51 +0700

    commit 7

--- design/main/views.py
+++ design/main/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request, 'main/index.html')
@@ -6,4 +7,14 @@ def index(request):
 @login_required
 def profile(request):
     return render(request, 'main/profile.html')
+
+def register(request):
+    return render(request, 'main/registration.html')
+
+def create_design_request(request):
+    return render(request, 'main/create.html')
+
+
+def is_admin(user):
+    return user.is_superuser
 
