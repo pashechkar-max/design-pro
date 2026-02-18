62a36ddc98d628d1487504b00c638f8412979d76
pashechkar-max <polinausakova921@gmail.com>
2026-02-18 22:40:34 +0700

    commit 5

--- design/main/models.py
+++ design/main/models.py
@@ -1,4 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='profile'
+    )
+    patronymic = models.CharField(
+        max_length=150,
+        verbose_name='Patronymic',
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profile'
+
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
 class Category(models.Model):
     name = models.CharField(
