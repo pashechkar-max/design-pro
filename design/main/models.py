c2be6d1c25d3208a8a48cfd57e84529e9530304e
pashechkar-max <polinausakova921@gmail.com>
2026-02-18 22:31:32 +0700

    commit 4

--- design/main/models.py
+++ design/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(
+        'Category name',
+        max_length=100,
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
