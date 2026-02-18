d3a55695bee740b310587894c616f05d6b03855d
pashechkar-max <polinausakova921@gmail.com>
2026-02-18 22:24:56 +0700

    commit 3

--- design/main/views.py
+++ design/main/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def index(request):
+    return render(request, 'main/index.html')
+
+@login_required
+def profile(request):
+    return render(request, 'main/profile.html')
+
